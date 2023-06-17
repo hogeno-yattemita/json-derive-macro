11e17c55d9a53bcc1a201641780dc67eb737a0ac
hogeno <masshi1108@gmail.com>
2023-06-17 19:25:10 +0900

    [update] json-derive

--- json-derive/src/lib.rs
+++ json-derive/src/lib.rs
@@ -5,12 +5,39 @@ use syn::{parse_macro_input, Data, DeriveInput};
 #[proc_macro_derive(json)]
 pub fn json_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let fields = match input.data {
-        Data::Struct(s) => s.fields.into_iter().map(|field| field.ident.unwrap().to_string()).collect::<Vec<_>>(),
-        Data::Enum(e) => e.variants.into_iter().map(|variant| variant.ident.to_string()).collect(),
-        Data::Union(u) => u.fields.named.into_iter().map(|field| field.ident.unwrap().to_string()).collect()
-    };
+    let name = input.ident;
 
-    panic!("\n {:#?}", fields);
-    quote!().into()
+    let out = match input.data {
+        Data::Struct(s) => {
+            let fields = s.fields.into_iter().map(|field| field.ident.unwrap());
+            quote! {
+                impl json_trait::Json for #name {
+                    fn to_json(&self) -> String {
+                        let mut json = "{ ".to_string();
+                        #(
+                            json.push_str(&format!("\"{}\": {}, ", stringify!(#fields), json_trait::Json::to_json(&self.#fields)));
+                        )*
+                        json.remove(json.len() - 2); // remove trailling comma
+                        json.push('}');
+                        json
+                    }
+                }
+            }
+        },
+        Data::Enum(e) => {
+            let variants = e.variants.into_iter().map(|variant| variant.ident);
+            quote! {
+                impl json_trait::Json for #name {
+                    fn to_json(&self) -> String {
+                        match &self {
+                            #(Self::#variants => format!("\"{}\"", stringify!(#variants)),)*
+                        }
+                    }
+                }
+            }
+        }
+
+        _ => todo!(),
+    };
+    out.into()
 }
